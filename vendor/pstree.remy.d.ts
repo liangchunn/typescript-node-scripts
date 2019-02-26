ee2eaf834b8f8255a4b466d88e603d022356f448
Liang Chun <liangchunn@me.com>
2019-02-26 17:21:54 +0100

    fix(vendor): fix spelling

--- vendor/pstree.remy.d.ts
+++ vendor/pstree.remy.d.ts
@@ -2,6 +2,6 @@ declare module 'pstree.remy' {
   export const hasPS: boolean
   export default function main(
     pid: string,
-    callback: (erorr: Error, children: string[]) => void
+    callback: (error: Error, children: string[]) => void
   ): void
 }
